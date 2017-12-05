b0ccc98300dd522bb1186dbd8ed3ba4de681fdf5
Tanjim Hossain <tanjim@revesoft.com>
2017-12-05 23:20:59 +0600

    Initial commit

---
+++ Hello-World-Swift/Hello-World-Swift/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Hello-World-Swift
+//
+//  Created by Tanjim Hossain on 12/5/17.
+//  Copyright © 2017 Tanjim Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var inputLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
