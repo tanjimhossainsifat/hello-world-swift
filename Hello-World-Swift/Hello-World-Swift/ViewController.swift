c6d4ce0c1dea0c020651c4bf91dac92043edd6fb
Tanjim Hossain <tanjim@revesoft.com>
2017-12-30 21:37:20 +0600

    These changes are added: 1. An imageView added 2. Tap gesture action added to the imageView 3. When imageView is tapped, photoLibrary is shown in imagePicker 4. If image selected, image is shown in imageView and outputLabel's text is updated

--- Hello-World-Swift/Hello-World-Swift/ViewController.swift
+++ Hello-World-Swift/Hello-World-Swift/ViewController.swift
@@ -8,17 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Outlets
     @IBOutlet weak var inputLabel: UILabel!
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    
+    var fullName:String = ""
     
     //MARK: UIViewController Overriden methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.photoView.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,10 +36,42 @@ class ViewController: UIViewController,UITextFieldDelegate {
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
         if let name = textField.text {
-            self.outputLabel.text = "Welcome " + name
+            fullName = name
+            self.outputLabel.text = "Welcome \(fullName) \n Please set your photo"
+            self.photoView.isHidden = false
         }
     }
-
-
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        self.inputTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: UIImagePickerControllerDelegate Methods
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary with Image, but got followng: \(info)")
+        }
+        
+        self.photoView.image = selectedImage
+        
+        self.outputLabel.text = "Thank you \(fullName)"
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
