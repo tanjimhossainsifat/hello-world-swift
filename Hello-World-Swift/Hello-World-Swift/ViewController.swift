a1b0aceb3f8c25543b63edb22cfe1c7f93dc7e58
Tanjim Hossain <tanjim@revesoft.com>
2017-12-30 20:17:59 +0600

    These changes are added: 1. Outlets for labels and textfield added 2. UITextFieldDelegate methods added 3. When a name is typed at textfield, an Welcome message is shown at OutputLabel

--- Hello-World-Swift/Hello-World-Swift/ViewController.swift
+++ Hello-World-Swift/Hello-World-Swift/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
     
+    //MARK: Outlets
     @IBOutlet weak var inputLabel: UILabel!
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var outputLabel: UILabel!
     
+    //MARK: UIViewController Overriden methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +23,17 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: UITextFieldDelegate Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let name = textField.text {
+            self.outputLabel.text = "Welcome " + name
+        }
     }
 
 
